935b3ea0d2ed3cd73ab3a08078025f6e81f9c542
yorkglez <bobo_12yors@hotmail.com>
2018-04-13 23:25:33 -0500

    sd

---
+++ app/src/main/java/com/tecmm/tala/listviewproductos/Addproduct.java
@@ -0,0 +1,38 @@
+package com.tecmm.tala.listviewproductos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class Addproduct extends AppCompatActivity {
+
+    private Spinner SpinnerProduct;
+    private TextView Txtname;
+    private Button Btnsave;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addproduct);
+        SpinnerProduct =(Spinner) findViewById(R.id.SpinnerProducts);
+        Txtname = (TextView) findViewById(R.id.Txtname);
+        String Options[] = {"Bebe","Auto","Electronicos","Peliculas","Ropa","Zapatos","Deportes","Hogar","Industria","Juegos","Libros","Mascotas","Musica"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, Options);
+        SpinnerProduct.setAdapter(adapter );
+        Btnsave = (Button) findViewById(R.id.Btnsave);
+        Btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Main();
+            }
+        });
+    }
+    public void Main(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/tecmm/tala/listviewproductos/MainActivity.java
+++ app/src/main/java/com/tecmm/tala/listviewproductos/MainActivity.java
@@ -1,10 +1,13 @@
 package com.tecmm.tala.listviewproductos;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,6 +16,7 @@ import java.lang.reflect.Array;
 public class MainActivity extends AppCompatActivity {
     private TextView txt;
     private ListView list;
+    private FloatingActionButton Btnadd;
     private String productos[] = {"Computadora", "Mouse", "Dulces", "Hojas",
             "Lapices", "Lentes","Reloj", "Cuchara", "Celular", "Mesa", "Refrigerador",
             "Horno", "Audifonos"};
@@ -34,5 +38,17 @@ public class MainActivity extends AppCompatActivity {
                 txt.setText("Categoria elegido: " + categoria[i]);
             }
         });
+        Btnadd = (FloatingActionButton) findViewById(R.id.Btnadd);
+        Btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenAddproduct();
+            }
+        });
+    }
+    public void OpenAddproduct(){
+        Intent intent = new Intent(this, Addproduct.class);
+        startActivity(intent);
+
     }
 }
